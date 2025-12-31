09942da131e884ef89d5e877f9dfa7c5a5fb388c
Jialing Xu <202312010016@nuist.edu.cn>
2025-12-31 11:43:46 +0800

    Update CODE-1.2.py

--- CODE-1.2.py
+++ CODE-1.2.py
@@ -1,5 +1,12 @@
 import numpy as np
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
 import xarray as xr
+import matplotlib as mpl
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from palettable.colorbrewer.diverging import RdBu_11_r
+import regionmask
+import geopandas as gp
 from math import pi, sin, cos, tan, log
 
 # 定义目标网格的经度和纬度范围
@@ -16,7 +23,7 @@ def bilinear_interpolation(data):
 #     lon_name = 'lon'
 #     dataset = dataset.assign_coords({lon_name: (((dataset[lon_name] + 180) % 360) - 180)})
 #     dataset = dataset.sortby(lon_name)
-
+    
 #     if 180 not in dataset[lon_name].values:
 #         minus_180_data = dataset.sel({lon_name: -180}, method="nearest")
 #         new_lon_values = np.append(dataset[lon_name].values, 180)
@@ -24,7 +31,7 @@ def bilinear_interpolation(data):
 #         dataset = dataset.reindex(
 #             {lon_name: new_lon_values_sorted},
 #             method="nearest", 
-#             tolerance=10 
+#             tolerance=10
 #         )
 #         dataset.loc[{lon_name: 180}] = minus_180_data
     
@@ -36,7 +43,6 @@ def adjust_longitude(dataset):
     lon_name = 'lon'
     dataset = dataset.assign_coords({lon_name: (((dataset[lon_name] + 180) % 360) - 180)})
     dataset = dataset.sortby(lon_name)
-    
     lon_min = dataset[lon_name].min().item()
     lon_max = dataset[lon_name].max().item()
     
@@ -45,9 +51,9 @@ def adjust_longitude(dataset):
         right_data = dataset.sel({lon_name: lon_max}, method="nearest")
         new_lon_values = np.unique(
             np.concatenate([
-                [-180],
+                [-180], 
                 dataset[lon_name].values,
-                [180] 
+                [180]  
             ])
         )
         dataset = dataset.reindex(
@@ -60,124 +66,148 @@ def adjust_longitude(dataset):
     
     return dataset
 
-
-# ##### 降水变化
-# f_prCTL = r"E:\CMIP6\UKESM\pr_1pctCO2_1850-1999.nc"
-# prCTLq30 = xr.open_dataset(f_prCTL).pr.sel(time=slice('1851-01', '1880-12')) *60*60*24
-# prCTLh30 = xr.open_dataset(f_prCTL).pr.sel(time=slice('1955-01', '1984-12')) *60*60*24
-# f_prrad = r"E:\CMIP6\UKESM\pr_1pctCO2-rad_1850-1999.nc"
-# prradq30 = xr.open_dataset(f_prrad).pr.sel(time=slice('1851-01', '1880-12')) *60*60*24
-# prradh30 = xr.open_dataset(f_prrad).pr.sel(time=slice('1955-01', '1984-12')) *60*60*24
-# f_prbgc = r"E:\CMIP6\UKESM\pr_1pctCO2-bgc_1850-1999.nc"
-# prbgcq30 = xr.open_dataset(f_prbgc).pr.sel(time=slice('1851-01', '1880-12')) *60*60*24
-# prbgch30 = xr.open_dataset(f_prbgc).pr.sel(time=slice('1955-01', '1984-12')) *60*60*24
-
-# month_q30 = prCTLq30.time.dt.days_in_month
-# month_h30 = prCTLh30.time.dt.days_in_month
-
-# prCTLq30_total = prCTLq30 * month_q30
-# prCTLh30_total = prCTLh30 * month_h30
-# prradq30_total = prradq30 * month_q30
-# prradh30_total = prradh30 * month_h30
-# prbgcq30_total = prbgcq30 * month_q30
-# prbgch30_total = prbgch30 * month_h30
-
-# PCTL = prCTLh30_total.groupby('time.year').sum(dim='time').mean(dim='year') - prCTLq30_total.groupby('time.year').sum(dim='time').mean(dim='year')
-# Prad = prradh30_total.groupby('time.year').sum(dim='time').mean(dim='year') - prradq30_total.groupby('time.year').sum(dim='time').mean(dim='year')
-# Pbgc = prbgch30_total.groupby('time.year').sum(dim='time').mean(dim='year') - prbgcq30_total.groupby('time.year').sum(dim='time').mean(dim='year')
-
-# xianghu = PCTL - Prad - Pbgc
+def save_as_netcdf(data, filename):
+    data_ad = adjust_longitude(data)
+    rdata = bilinear_interpolation(data_ad)
+    rdata.to_netcdf(filename)
 
 
 
-# ##### 温度变化
-# f_TmaxCTL = r"E:\CMIP6\ACCESS\tasmax_Amon_ACCESS-ESM1-5_1pctCO2_r1i1p1f1_gn_010101-025012.nc"
-# TmaxCTLq30 = xr.open_dataset(f_TmaxCTL).tasmax.sel(time=slice('0102-01', '0131-12')) - 273.16
-# TmaxCTLh30 = xr.open_dataset(f_TmaxCTL).tasmax.sel(time=slice('0206-01', '0235-12')) - 273.16
-# f_TminCTL = r"E:\CMIP6\ACCESS\tasmin_Amon_ACCESS-ESM1-5_1pctCO2_r1i1p1f1_gn_010101-025012.nc"
-# TminCTLq30 = xr.open_dataset(f_TminCTL).tasmin.sel(time=slice('0102-01', '0131-12')) - 273.16
-# TminCTLh30 = xr.open_dataset(f_TminCTL).tasmin.sel(time=slice('0206-01', '0235-12')) - 273.16
+##### 降水
+f_PCTL = r"E:\CMIP6\UKESM\pr_1pctCO2_1850-1999.nc"
+PCTLq30 = xr.open_dataset(f_PCTL).pr.sel(time=slice('1851-01', '1880-12')) *60*60*24
+PCTLh30 = xr.open_dataset(f_PCTL).pr.sel(time=slice('1955-01', '1984-12')) *60*60*24
+f_Prad = r"E:\CMIP6\UKESM\pr_1pctCO2-rad_1850-1999.nc"
+Pradq30 = xr.open_dataset(f_Prad).pr.sel(time=slice('1851-01', '1880-12')) *60*60*24
+Pradh30 = xr.open_dataset(f_Prad).pr.sel(time=slice('1955-01', '1984-12')) *60*60*24
+f_Pbgc = r"E:\CMIP6\UKESM\pr_1pctCO2-bgc_1850-1999.nc"
+Pbgcq30 = xr.open_dataset(f_Pbgc).pr.sel(time=slice('1851-01', '1880-12')) *60*60*24
+Pbgch30 = xr.open_dataset(f_Pbgc).pr.sel(time=slice('1955-01', '1984-12')) *60*60*24
 
-# TCTLq30 = (TmaxCTLq30 + TminCTLq30)/2
-# TCTLh30 = (TmaxCTLh30 + TminCTLh30)/2
+month_q30 = PCTLq30.time.dt.days_in_month
+month_h30 = PCTLh30.time.dt.days_in_month
 
-# f_Tmaxrad = r"E:\CMIP6\ACCESS\tasmax_Amon_ACCESS-ESM1-5_1pctCO2-rad_r1i1p1f1_gn_010101-025012.nc"
-# Tmaxradq30 = xr.open_dataset(f_Tmaxrad).tasmax.sel(time=slice('0102-01', '0131-12')) - 273.16
-# Tmaxradh30 = xr.open_dataset(f_Tmaxrad).tasmax.sel(time=slice('0206-01', '0235-12')) - 273.16
-# f_Tminrad = r"E:\CMIP6\ACCESS\tasmin_Amon_ACCESS-ESM1-5_1pctCO2-rad_r1i1p1f1_gn_010101-025012.nc"
-# Tminradq30 = xr.open_dataset(f_Tminrad).tasmin.sel(time=slice('0102-01', '0131-12')) - 273.16
-# Tminradh30 = xr.open_dataset(f_Tminrad).tasmin.sel(time=slice('0206-01', '0235-12')) - 273.16
+PrCTLq30 = (PCTLq30 * month_q30).where(lambda x: x >= 0, 0)
+PrCTLh30 = (PCTLh30 * month_h30).where(lambda x: x >= 0, 0)
+Prradq30 = (Pradq30 * month_q30).where(lambda x: x >= 0, 0)
+Prradh30 = (Pradh30 * month_h30).where(lambda x: x >= 0, 0)
+Prbgcq30 = (Pbgcq30 * month_q30).where(lambda x: x >= 0, 0)
+Prbgch30 = (Pbgch30 * month_h30).where(lambda x: x >= 0, 0)
 
-# Tradq30 = (Tmaxradq30 + Tminradq30)/2
-# Tradh30 = (Tmaxradh30 + Tminradh30)/2
+months = PCTLh30['time.month'].values
 
-# f_Tmaxbgc = r"E:\CMIP6\ACCESS\tasmax_Amon_ACCESS-ESM1-5_1pctCO2-bgc_r1i1p1f1_gn_010101-025012.nc"
-# Tmaxbgcq30 = xr.open_dataset(f_Tmaxbgc).tasmax.sel(time=slice('0102-01', '0131-12')) - 273.16
-# Tmaxbgch30 = xr.open_dataset(f_Tmaxbgc).tasmax.sel(time=slice('0206-01', '0235-12')) - 273.16
-# f_Tminbgc = r"E:\CMIP6\ACCESS\tasmin_Amon_ACCESS-ESM1-5_1pctCO2-bgc_r1i1p1f1_gn_010101-025012.nc"
-# Tminbgcq30 = xr.open_dataset(f_Tminbgc).tasmin.sel(time=slice('0102-01', '0131-12')) - 273.16
-# Tminbgch30 = xr.open_dataset(f_Tminbgc).tasmin.sel(time=slice('0206-01', '0235-12')) - 273.16
+PCTL = PrCTLh30 - PrCTLq30.groupby('time.month').mean('time').sel(month=months).values
+Prad = Prradh30 - Prradq30.groupby('time.month').mean('time').sel(month=months).values
+Pbgc = Prbgch30 - Prbgcq30.groupby('time.month').mean('time').sel(month=months).values
+Pint = PCTL - Prad - Pbgc
 
-# Tbgcq30 = (Tmaxbgcq30 + Tminbgcq30)/2
-# Tbgch30 = (Tmaxbgch30 + Tminbgch30)/2
+Prad = Prad.groupby('time.year').sum(dim='time').mean(dim='year')
+Pbgc = Pbgc.groupby('time.year').sum(dim='time').mean(dim='year')
+Pint = Pint.groupby('time.year').sum(dim='time').mean(dim='year')
 
-# TCTL = TCTLh30.mean(dim='time') - TCTLq30.mean(dim='time')
-# Trad = Tradh30.mean(dim='time') - Tradq30.mean(dim='time')
-# Tbgc = Tbgch30.mean(dim='time') - Tbgcq30.mean(dim='time')
-
-# xianghu = TCTL - Trad - Tbgc
-
-
-
-##### 净辐射变化
-f_rssCTL = r"E:\CMIP6\UKESM\rss_1pctCO2_1850-1999.nc"
-RnsCTLq30 = xr.open_dataset(f_rssCTL).rss.sel(time=slice('1851-01', '1880-12'))
-RnsCTLh30 = xr.open_dataset(f_rssCTL).rss.sel(time=slice('1955-01', '1984-12'))
-f_rlsCTL = r"E:\CMIP6\UKESM\rls_1pctCO2_1850-1999.nc"
-RnlCTLq30 = xr.open_dataset(f_rlsCTL).rls.sel(time=slice('1851-01', '1880-12'))
-RnlCTLh30 = xr.open_dataset(f_rlsCTL).rls.sel(time=slice('1955-01', '1984-12'))
-
-month_q30 = RnsCTLq30.time.dt.days_in_month
-month_h30 = RnsCTLh30.time.dt.days_in_month
-
-RnCTLq30 = (RnsCTLq30 + RnlCTLq30) * 0.0864 * month_q30
-RnCTLh30 = (RnsCTLh30 + RnlCTLh30) * 0.0864 * month_h30
-RNCTLq30 = xr.where(RnCTLq30 < 0, 0, RnCTLq30)
-RNCTLh30 = xr.where(RnCTLh30 < 0, 0, RnCTLh30)
-
-f_rssrad = r"E:\CMIP6\UKESM\rss_1pctCO2-rad_1850-1999.nc"
-Rnsradq30 = xr.open_dataset(f_rssrad).rss.sel(time=slice('1851-01', '1880-12'))
-Rnsradh30 = xr.open_dataset(f_rssrad).rss.sel(time=slice('1955-01', '1984-12'))
-f_rlsrad = r"E:\CMIP6\UKESM\rls_1pctCO2-rad_1850-1999.nc"
-Rnlradq30 = xr.open_dataset(f_rlsrad).rls.sel(time=slice('1851-01', '1880-12'))
-Rnlradh30 = xr.open_dataset(f_rlsrad).rls.sel(time=slice('1955-01', '1984-12'))
-
-Rnradq30 = (Rnsradq30 + Rnlradq30) * 0.0864 * month_q30
-Rnradh30 = (Rnsradh30 + Rnlradh30) * 0.0864 * month_h30
-RNradq30 = xr.where(Rnradq30 < 0, 0, Rnradq30)
-RNradh30 = xr.where(Rnradh30 < 0, 0, Rnradh30)
-
-f_rssbgc = r"E:\CMIP6\UKESM\rss_1pctCO2-bgc_1850-1999.nc"
-Rnsbgcq30 = xr.open_dataset(f_rssbgc).rss.sel(time=slice('1851-01', '1880-12'))
-Rnsbgch30 = xr.open_dataset(f_rssbgc).rss.sel(time=slice('1955-01', '1984-12'))
-f_rlsbgc = r"E:\CMIP6\UKESM\rls_1pctCO2-bgc_1850-1999.nc"
-Rnlbgcq30 = xr.open_dataset(f_rlsbgc).rls.sel(time=slice('1851-01', '1880-12'))
-Rnlbgch30 = xr.open_dataset(f_rlsbgc).rls.sel(time=slice('1955-01', '1984-12'))
-
-Rnbgcq30 = (Rnsbgcq30 + Rnlbgcq30) * 0.0864 * month_q30
-Rnbgch30 = (Rnsbgch30 + Rnlbgch30) * 0.0864 * month_h30
-RNbgcq30 = xr.where(Rnbgcq30 < 0, 0, Rnbgcq30)
-RNbgch30 = xr.where(Rnbgch30 < 0, 0, Rnbgch30)
-
-RNCTL = RNCTLh30.groupby('time.year').sum(dim='time').mean(dim='year') - RNCTLq30.groupby('time.year').sum(dim='time').mean(dim='year')
-RNrad = RNradh30.groupby('time.year').sum(dim='time').mean(dim='year') - RNradq30.groupby('time.year').sum(dim='time').mean(dim='year')
-RNbgc = RNbgch30.groupby('time.year').sum(dim='time').mean(dim='year') - RNbgcq30.groupby('time.year').sum(dim='time').mean(dim='year')
-
-xianghu = RNCTL - RNrad - RNbgc
+save_as_netcdf(Prad, r"E:\CMIP6\UKESM\P_rad.nc")
+save_as_netcdf(Pbgc, r"E:\CMIP6\UKESM\P_bgc.nc")
+save_as_netcdf(Pint, r"E:\CMIP6\UKESM\P_int.nc")
 
 
 
-# ##### 相对湿度变化
+# ##### 温度
+# f_TmaxCTL = r"E:\CMIP6\UKESM\tasmax_1pctCO2_1850-1999.nc"
+# TmaxCTLq30 = xr.open_dataset(f_TmaxCTL).tasmax.sel(time=slice('1851-01', '1880-12')) - 273.16
+# TmaxCTLh30 = xr.open_dataset(f_TmaxCTL).tasmax.sel(time=slice('1955-01', '1984-12')) - 273.16
+# f_Tmaxrad = r"E:\CMIP6\UKESM\tasmax_1pctCO2-rad_1850-1999.nc"
+# Tmaxradq30 = xr.open_dataset(f_Tmaxrad).tasmax.sel(time=slice('1851-01', '1880-12')) - 273.16
+# Tmaxradh30 = xr.open_dataset(f_Tmaxrad).tasmax.sel(time=slice('1955-01', '1984-12')) - 273.16
+# f_Tmaxbgc = r"E:\CMIP6\UKESM\tasmax_1pctCO2-bgc_1850-1999.nc"
+# Tmaxbgcq30 = xr.open_dataset(f_Tmaxbgc).tasmax.sel(time=slice('1851-01', '1880-12')) - 273.16
+# Tmaxbgch30 = xr.open_dataset(f_Tmaxbgc).tasmax.sel(time=slice('1955-01', '1984-12')) - 273.16
+
+# f_TminCTL = r"E:\CMIP6\UKESM\tasmin_1pctCO2_1850-1999.nc"
+# TminCTLq30 = xr.open_dataset(f_TminCTL).tasmin.sel(time=slice('1851-01', '1880-12')) - 273.16
+# TminCTLh30 = xr.open_dataset(f_TminCTL).tasmin.sel(time=slice('1955-01', '1984-12')) - 273.16
+# f_Tminrad = r"E:\CMIP6\UKESM\tasmin_1pctCO2-rad_1850-1999.nc"
+# Tminradq30 = xr.open_dataset(f_Tminrad).tasmin.sel(time=slice('1851-01', '1880-12')) - 273.16
+# Tminradh30 = xr.open_dataset(f_Tminrad).tasmin.sel(time=slice('1955-01', '1984-12')) - 273.16
+# f_Tminbgc = r"E:\CMIP6\UKESM\tasmin_1pctCO2-bgc_1850-1999.nc"
+# Tminbgcq30 = xr.open_dataset(f_Tminbgc).tasmin.sel(time=slice('1851-01', '1880-12')) - 273.16
+# Tminbgch30 = xr.open_dataset(f_Tminbgc).tasmin.sel(time=slice('1955-01', '1984-12')) - 273.16
+
+# TCTLq30 = 0.5 * (TmaxCTLq30 + TminCTLq30)
+# TCTLh30 = 0.5 * (TmaxCTLh30 + TminCTLh30)
+# Tradq30 = 0.5 * (Tmaxradq30 + Tminradq30)
+# Tradh30 = 0.5 * (Tmaxradh30 + Tminradh30)
+# Tbgcq30 = 0.5 * (Tmaxbgcq30 + Tminbgcq30)
+# Tbgch30 = 0.5 * (Tmaxbgch30 + Tminbgch30)
+
+# months = TCTLh30['time.month'].values
+
+# TCTL = TCTLh30 - TCTLq30.groupby('time.month').mean('time').sel(month=months).values
+# Trad = Tradh30 - Tradq30.groupby('time.month').mean('time').sel(month=months).values
+# Tbgc = Tbgch30 - Tbgcq30.groupby('time.month').mean('time').sel(month=months).values
+# Tint = TCTL - Trad - Tbgc
+
+# Trad = Trad.mean(dim='time')
+# Tbgc = Tbgc.mean(dim='time')
+# Tint = Tint.mean(dim='time')
+
+# save_as_netcdf(Trad, r"E:\CMIP6\UKESM\T_rad.nc")
+# save_as_netcdf(Tbgc, r"E:\CMIP6\UKESM\T_bgc.nc")
+# save_as_netcdf(Tint, r"E:\CMIP6\UKESM\T_int.nc")
+
+
+
+# ##### 净辐射
+# f_rssCTL = r"E:\CMIP6\UKESM\rss_1pctCO2_1850-1999.nc"
+# RnsCTLq30 = xr.open_dataset(f_rssCTL).rss.sel(time=slice('1851-01', '1880-12'))
+# RnsCTLh30 = xr.open_dataset(f_rssCTL).rss.sel(time=slice('1955-01', '1984-12'))
+# f_rlsCTL = r"E:\CMIP6\UKESM\rls_1pctCO2_1850-1999.nc"
+# RnlCTLq30 = xr.open_dataset(f_rlsCTL).rls.sel(time=slice('1851-01', '1880-12'))
+# RnlCTLh30 = xr.open_dataset(f_rlsCTL).rls.sel(time=slice('1955-01', '1984-12'))
+
+# month_q30 = RnsCTLq30.time.dt.days_in_month
+# month_h30 = RnsCTLh30.time.dt.days_in_month
+
+# RNCTLq30 = ((RnsCTLq30 + RnlCTLq30) * 0.0864 * month_q30).where(lambda x: x >= 0, 0)
+# RNCTLh30 = ((RnsCTLh30 + RnlCTLh30) * 0.0864 * month_h30).where(lambda x: x >= 0, 0)
+
+# f_rssrad = r"E:\CMIP6\UKESM\rss_1pctCO2-rad_1850-1999.nc"
+# Rnsradq30 = xr.open_dataset(f_rssrad).rss.sel(time=slice('1851-01', '1880-12'))
+# Rnsradh30 = xr.open_dataset(f_rssrad).rss.sel(time=slice('1955-01', '1984-12'))
+# f_rlsrad = r"E:\CMIP6\UKESM\rls_1pctCO2-rad_1850-1999.nc"
+# Rnlradq30 = xr.open_dataset(f_rlsrad).rls.sel(time=slice('1851-01', '1880-12'))
+# Rnlradh30 = xr.open_dataset(f_rlsrad).rls.sel(time=slice('1955-01', '1984-12'))
+
+# RNradq30 = ((Rnsradq30 + Rnlradq30) * 0.0864 * month_q30).where(lambda x: x >= 0, 0)
+# RNradh30 = ((Rnsradh30 + Rnlradh30) * 0.0864 * month_h30).where(lambda x: x >= 0, 0)
+
+# f_rssbgc = r"E:\CMIP6\UKESM\rss_1pctCO2-bgc_1850-1999.nc"
+# Rnsbgcq30 = xr.open_dataset(f_rssbgc).rss.sel(time=slice('1851-01', '1880-12'))
+# Rnsbgch30 = xr.open_dataset(f_rssbgc).rss.sel(time=slice('1955-01', '1984-12'))
+# f_rlsbgc = r"E:\CMIP6\UKESM\rls_1pctCO2-bgc_1850-1999.nc"
+# Rnlbgcq30 = xr.open_dataset(f_rlsbgc).rls.sel(time=slice('1851-01', '1880-12'))
+# Rnlbgch30 = xr.open_dataset(f_rlsbgc).rls.sel(time=slice('1955-01', '1984-12'))
+
+# RNbgcq30 = ((Rnsbgcq30 + Rnlbgcq30) * 0.0864 * month_q30).where(lambda x: x >= 0, 0)
+# RNbgch30 = ((Rnsbgch30 + Rnlbgch30) * 0.0864 * month_h30).where(lambda x: x >= 0, 0)
+
+# months = RNCTLh30['time.month'].values
+
+# RNCTL = RNCTLh30 - RNCTLq30.groupby('time.month').mean('time').sel(month=months).values
+# RNrad = RNradh30 - RNradq30.groupby('time.month').mean('time').sel(month=months).values
+# RNbgc = RNbgch30 - RNbgcq30.groupby('time.month').mean('time').sel(month=months).values
+# RNint = RNCTL - RNrad - RNbgc
+
+# RNrad = RNrad.groupby('time.year').sum(dim='time').mean(dim='year')
+# RNbgc = RNbgc.groupby('time.year').sum(dim='time').mean(dim='year')
+# RNint = RNint.groupby('time.year').sum(dim='time').mean(dim='year')
+
+# save_as_netcdf(RNrad, r"E:\CMIP6\UKESM\RN_rad.nc")
+# save_as_netcdf(RNbgc, r"E:\CMIP6\UKESM\RN_bgc.nc")
+# save_as_netcdf(RNint, r"E:\CMIP6\UKESM\RN_int.nc")
+
+
+
+# ##### 相对湿度
 # f_RHCTL = r"E:\CMIP6\UKESM\hurs_1pctCO2_1850-1999.nc"
 # RHCTLq30 = xr.open_dataset(f_RHCTL).hurs.sel(time=slice('1851-01', '1880-12'))
 # RHCTLh30 = xr.open_dataset(f_RHCTL).hurs.sel(time=slice('1955-01', '1984-12'))
@@ -188,35 +218,46 @@ xianghu = RNCTL - RNrad - RNbgc
 # RHbgcq30 = xr.open_dataset(f_RHbgc).hurs.sel(time=slice('1851-01', '1880-12'))
 # RHbgch30 = xr.open_dataset(f_RHbgc).hurs.sel(time=slice('1955-01', '1984-12'))
 
-# RHCTL = RHCTLh30.mean(dim='time') - RHCTLq30.mean(dim='time')
-# RHrad = RHradh30.mean(dim='time') - RHradq30.mean(dim='time')
-# RHbgc = RHbgch30.mean(dim='time') - RHbgcq30.mean(dim='time')
+# months = RHCTLh30['time.month'].values
 
-# xianghu = RHCTL - RHrad - RHbgc
+# RHCTL = RHCTLh30 - RHCTLq30.groupby('time.month').mean('time').sel(month=months).values
+# RHrad = RHradh30 - RHradq30.groupby('time.month').mean('time').sel(month=months).values
+# RHbgc = RHbgch30 - RHbgcq30.groupby('time.month').mean('time').sel(month=months).values
+# RHint = RHCTL - RHrad - RHbgc
 
+# RHrad = RHrad.mean(dim='time')
+# RHbgc = RHbgc.mean(dim='time')
+# RHint = RHint.mean(dim='time')
 
-
-# ##### 风速变化
-# f_UCTL = r"E:\CMIP6\ACCESS\sfcWind_Amon_ACCESS-ESM1-5_1pctCO2_r1i1p1f1_gn_010101-025012.nc"
-# UCTLq30 = xr.open_dataset(f_UCTL).sfcWind.sel(time=slice('0102-01', '0131-12')) * (4.87 / log(67.8 * 10 - 5.42))
-# UCTLh30 = xr.open_dataset(f_UCTL).sfcWind.sel(time=slice('0206-01', '0235-12')) * (4.87 / log(67.8 * 10 - 5.42))
-# f_Urad = r"E:\CMIP6\ACCESS\sfcWind_Amon_ACCESS-ESM1-5_1pctCO2-rad_r1i1p1f1_gn_010101-025012.nc"
-# Uradq30 = xr.open_dataset(f_Urad).sfcWind.sel(time=slice('0102-01', '0131-12')) * (4.87 / log(67.8 * 10 - 5.42))
-# Uradh30 = xr.open_dataset(f_Urad).sfcWind.sel(time=slice('0206-01', '0235-12')) * (4.87 / log(67.8 * 10 - 5.42))
-# f_Ubgc = r"E:\CMIP6\ACCESS\sfcWind_Amon_ACCESS-ESM1-5_1pctCO2-bgc_r1i1p1f1_gn_010101-025012.nc"
-# Ubgcq30 = xr.open_dataset(f_Ubgc).sfcWind.sel(time=slice('0102-01', '0131-12')) * (4.87 / log(67.8 * 10 - 5.42))
-# Ubgch30 = xr.open_dataset(f_Ubgc).sfcWind.sel(time=slice('0206-01', '0235-12')) * (4.87 / log(67.8 * 10 - 5.42))
-
-# UCTL = UCTLh30.mean(dim='time') - UCTLq30.mean(dim='time')
-# Urad = Uradh30.mean(dim='time') - Uradq30.mean(dim='time')
-# Ubgc = Ubgch30.mean(dim='time') - Ubgcq30.mean(dim='time')
-
-# xianghu = UCTL - Urad - Ubgc
+# save_as_netcdf(RHrad, r"E:\CMIP6\UKESM\RH_rad.nc")
+# save_as_netcdf(RHbgc, r"E:\CMIP6\UKESM\RH_bgc.nc")
+# save_as_netcdf(RHint, r"E:\CMIP6\UKESM\RH_int.nc")
 
 
 
-data = adjust_longitude(xianghu)
-rdata = bilinear_interpolation(data)
-rdata.to_netcdf(r"E:\CMIP6\UKESM\RN_int.nc")
+# ##### 风速
+# f_UCTL = r"E:\CMIP6\UKESM\sfcWind_1pctCO2_1850-1999.nc"
+# UCTLq30 = xr.open_dataset(f_UCTL).sfcWind.sel(time=slice('1851-01', '1880-12')) * (4.87 / log(67.8 * 10 - 5.42))
+# UCTLh30 = xr.open_dataset(f_UCTL).sfcWind.sel(time=slice('1955-01', '1984-12')) * (4.87 / log(67.8 * 10 - 5.42))
+# f_Urad = r"E:\CMIP6\UKESM\sfcWind_1pctCO2-rad_1850-1999.nc"
+# Uradq30 = xr.open_dataset(f_Urad).sfcWind.sel(time=slice('1851-01', '1880-12')) * (4.87 / log(67.8 * 10 - 5.42))
+# Uradh30 = xr.open_dataset(f_Urad).sfcWind.sel(time=slice('1955-01', '1984-12')) * (4.87 / log(67.8 * 10 - 5.42))
+# f_Ubgc = r"E:\CMIP6\UKESM\sfcWind_1pctCO2-bgc_1850-1999.nc"
+# Ubgcq30 = xr.open_dataset(f_Ubgc).sfcWind.sel(time=slice('1851-01', '1880-12')) * (4.87 / log(67.8 * 10 - 5.42))
+# Ubgch30 = xr.open_dataset(f_Ubgc).sfcWind.sel(time=slice('1955-01', '1984-12')) * (4.87 / log(67.8 * 10 - 5.42))
 
+# months = UCTLh30['time.month'].values
+
+# UCTL = UCTLh30 - UCTLq30.groupby('time.month').mean('time').sel(month=months).values
+# Urad = Uradh30 - Uradq30.groupby('time.month').mean('time').sel(month=months).values
+# Ubgc = Ubgch30 - Ubgcq30.groupby('time.month').mean('time').sel(month=months).values
+# Uint = UCTL - Urad - Ubgc
+
+# Urad = Urad.mean(dim='time')
+# Ubgc = Ubgc.mean(dim='time')
+# Uint = Uint.mean(dim='time')
+
+# save_as_netcdf(Urad, r"E:\CMIP6\UKESM\U_rad.nc")
+# save_as_netcdf(Ubgc, r"E:\CMIP6\UKESM\U_bgc.nc")
+# save_as_netcdf(Uint, r"E:\CMIP6\UKESM\U_int.nc")
 
